bd1f703cf9e31febcdd1e1bc2a476b69ffa7ee54
Selina <31773266+selinagerosa@users.noreply.github.com>
2018-05-12 22:47:56 -0400

    minor emoji tweaks

--- emo.js
+++ emo.js
@@ -1,21 +1,22 @@
-
-setInterval( function() {
-  var temp = Math.floor(Math.random() * 151);
-  document.getElementById("e0"+temp).style.visibility ='visible';
-},1500);
-setInterval( function() {
-  var temp = Math.floor(Math.random() * 151);
-  document.getElementById("e0"+temp).style.visibility ='visible';
-},4000);
-setInterval( function() {
-  var temp = Math.floor(Math.random() * 151);
-  document.getElementById("e0"+temp).style.visibility ='visible';
-},700);
-document.body.addEventListener("click", function() {
-    var lst = document.getElementsByClassName("emoji");
-    var i;
-    for (i=0; i<lst.length;i++) {
-      lst[i].style.visibility="hidden";
+setTimeout( function() {
+  setInterval( function() {
+    var temp = Math.floor(Math.random() * 151);
+    document.getElementById("e0"+temp).style.visibility ='visible';
+  },1500);
+  setInterval( function() {
+    var temp = Math.floor(Math.random() * 151);
+    document.getElementById("e0"+temp).style.visibility ='visible';
+  },4000);
+  setInterval( function() {
+    var temp = Math.floor(Math.random() * 151);
+    document.getElementById("e0"+temp).style.visibility ='visible';
+  },700);
+  document.body.addEventListener("click", function() {
+      var lst = document.getElementsByClassName("emoji");
+      var i;
+      for (i=0; i<lst.length;i++) {
+        lst[i].style.visibility="hidden";
+      }
     }
-  }
-)
+  )
+},2000);
